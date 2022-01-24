e7175f7be612ece232ab299bc2df98a6019dc91c
shubhkhanna <khannashubh2001@gmail.com>
2022-01-24 15:50:19 +0530

    ft: update server.js with new imports

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const db = require("./config/db");
 const PORT = process.env.PORT || 5000;
+
+const { handleError, notFound } = require("./middlewares/errorMiddleware");
 
 // configure .env file
 dotenv.config();
+
+// connect to database
+db();
 
 // Initialize the app
 const app = express();
@@ -15,6 +21,10 @@ app.use(helmet());
 
 // log all API requests to the console
 app.use(morgan("dev"));
+
+// Handle errors
+app.use(handleError);
+app.use(notFound);
 
 // listen to port 5000
 app.listen(PORT, () => {
