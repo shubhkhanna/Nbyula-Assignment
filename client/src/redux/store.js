0489249aaf1a13c47521ccb20fcc4df5e61b19ac
shubhkhanna <khannashubh2001@gmail.com>
2022-01-27 13:43:38 +0530

    ft: combine reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,27 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 
-const reducer = combineReducers({});
+// Combine all reducers
+const reducer = combineReducers({
+  userRegister: userRegisterReducer,
+  userLogin: userLoginReducer,
+});
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+// Initial state of the user
+const userFromStorage = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userFromStorage },
+};
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
