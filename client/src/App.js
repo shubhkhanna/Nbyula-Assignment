13b736eac145356e0083fe88c94efddae191c849
shubhkhanna <khannashubh2001@gmail.com>
2022-01-27 17:45:21 +0530

    ft: add course page and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Auth from "./components/Auth";
+import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
+import CoursePage from "./pages/CoursePage";
+import Navbar from "./components/common/Navbar";
+
 const App = () => {
-  return <div>App Screen</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        {/* Protected Routes */}
+        <Route element={<Auth />}>
+          <Route path="/" element={<CoursePage />} />
+        </Route>
+
+        {/* Public Routes */}
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/pages/CoursePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoursePage = () => {
+  return <div>Course Page</div>;
+};
+
+export default CoursePage;
